dff2b220f896a70c0aff19fabcbed97738e44fb0
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-05-22 23:37:45 +0500

    refactor: update

--- gaming.club.admin/src/components/settings/equipment-settings.js
+++ gaming.club.admin/src/components/settings/equipment-settings.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { List, Datagrid, TextField, EditButton, Edit, TextInput, SimpleForm, Create, useRecordContext } from 'react-admin';
+import { List, Datagrid, TextField, EditButton, Edit, TextInput, SimpleForm, Create, useRecordContext, ReferenceInput, SelectInput, ReferenceArrayInput, FunctionField, AutocompleteInput, required } from 'react-admin';
 
 export const EquipmentList = () => (
     <List>
         <Datagrid>
             <TextField source="id" />
             <TextField source="serial_number" />
+            <TextField source="type_name" />
+            <TextField source="model_name" />
+            <TextField source="condition_name" />
             <EditButton />
         </Datagrid>
     </List>
@@ -16,19 +19,39 @@ const EquipmentTitle = () => {
     return <span>Post {record ? `"${record.title}"` : ''}</span>;
 };
 
-export const EquipmentEdit = () => (
-    <Edit title={<EquipmentTitle />}>
-        <SimpleForm>
-            <TextInput disabled source="id" />
-            <TextInput source="serial_number" />
-        </SimpleForm>
-    </Edit>
-);
+export const EquipmentEdit = () => {
+    return (
+        <Edit title={<EquipmentTitle />}>
+            <SimpleForm>
+                <TextInput disabled source="id" />
+                <TextInput source="serial_number" />
+                <ReferenceInput source="modelId" reference="equipments/model">
+                    <SelectInput optionText="name" />
+                </ReferenceInput>
+                <ReferenceInput source="typeId" reference="equipments/type">
+                    <SelectInput optionText="name" />
+                </ReferenceInput>
+                <ReferenceInput source="conditionId" reference="equipments/condition">
+                    <SelectInput optionText="name" />
+                </ReferenceInput>
+            </SimpleForm>
+        </Edit>
+    );
+}
 
 export const EquipmentCreate = () => (
     <Create title="Create a Post">
         <SimpleForm>
             <TextInput source="serial_number" />
+            <ReferenceInput source="modelId" reference="equipments/model">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <ReferenceInput source="typeId" reference="equipments/type">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <ReferenceInput source="conditionId" reference="equipments/condition">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
         </SimpleForm>
     </Create>
 );

38d69c69ad68d67a4d38c704d0a6573aee7047d3
FremyXS <70861067+FremyXS@users.noreply.github.com>
2023-05-23 23:25:38 +0500

    refactor: reservation

--- gaming.club/src/reservation/reservation.controller.ts
+++ gaming.club/src/reservation/reservation.controller.ts
@@ -11,27 +11,31 @@ import {
 import { ReservationService } from './reservation.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateStatusDto } from './dto/create-status.dto';
+import { UpdateStatusDto } from './dto/update-status.dto';
 
-@Controller('reservation')
+@ApiTags('Reservations')
+@Controller('reservations')
 export class ReservationController {
   constructor(private readonly reservationService: ReservationService) {}
 
-  @Post()
+  @Post('reservation')
   async create(@Body() createReservationDto: CreateReservationDto) {
     return this.reservationService.create(createReservationDto);
   }
 
-  @Get()
+  @Get('reservation')
   async findAll() {
     return this.reservationService.findAll();
   }
 
-  @Get(':id')
+  @Get('reservation/:id')
   async findOne(@Param('id') id: string) {
     return this.reservationService.findOne(+id);
   }
 
-  @Put(':id')
+  @Put('reservation/:id')
   async update(
     @Param('id') id: string,
     @Body() updateReservationDto: UpdateReservationDto,
@@ -39,8 +43,36 @@ export class ReservationController {
     return this.reservationService.update(+id, updateReservationDto);
   }
 
-  @Delete(':id')
+  @Delete('reservation/:id')
   async remove(@Param('id') id: string) {
     return this.reservationService.remove(+id);
+  }
+
+  @Post('status')
+  async statusCreate(@Body() createStatusDto: CreateStatusDto) {
+    return this.reservationService.statusCreate(createStatusDto);
+  }
+
+  @Get('status')
+  async statusFindAll() {
+    return this.reservationService.statusFindAll();
+  }
+
+  @Get('status/:id')
+  async statusFindOne(@Param('id') id: string) {
+    return this.reservationService.statusFindOne(+id);
+  }
+
+  @Put('status/:id')
+  async statusUpdate(
+    @Param('id') id: string,
+    @Body() updateStatusDto: UpdateStatusDto,
+  ) {
+    return this.reservationService.statusUpdate(+id, updateStatusDto);
+  }
+
+  @Delete('status/:id')
+  async statusRemove(@Param('id') id: string) {
+    return this.reservationService.statusRemove(+id);
   }
 }
--- gaming.club/src/reservation/reservation.service.ts
+++ gaming.club/src/reservation/reservation.service.ts
@@ -1,26 +1,140 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Status } from './entities/status.entity';
+import { Repository } from 'typeorm';
+import { Reservation } from './entities/reservation.entity';
+import { CreateStatusDto } from './dto/create-status.dto';
+import { UpdateStatusDto } from './dto/update-status.dto';
 
 @Injectable()
 export class ReservationService {
-  create(createReservationDto: CreateReservationDto) {
-    return 'This action adds a new reservation';
+  constructor(
+    @InjectRepository(Status)
+    private readonly statusRepository: Repository<Status>,
+    @InjectRepository(Reservation)
+    private readonly reservationRepository: Repository<Reservation>,
+  ) {}
+
+  //reservation
+  async create(createReservationDto: CreateReservationDto) {
+    const status = await this.statusRepository.findOneBy({
+      id: createReservationDto.statusId,
+    });
+
+    if (!status) {
+      throw new NotFoundException(
+        `Status whit id: ${createReservationDto.statusId} not found`,
+      );
+    }
+
+    const newReservation = this.reservationRepository.create({
+      ...createReservationDto,
+    });
+
+    return await this.reservationRepository.save(newReservation);
   }
 
-  findAll() {
-    return `This action returns all reservation`;
+  async findAll() {
+    return await this.reservationRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} reservation`;
+  async findOne(id: number) {
+    return await this.reservationRepository.findOneBy({
+      id: id,
+    });
   }
 
-  update(id: number, updateReservationDto: UpdateReservationDto) {
+  async update(id: number, updateReservationDto: UpdateReservationDto) {
+    const reservation = this.reservationRepository.findOneBy({
+      id: id,
+    });
+
+    if (!reservation) {
+      throw new NotFoundException(`Reservation whit id: ${id} not found`);
+    }
+
+    const status = await this.statusRepository.findOneBy({
+      id: updateReservationDto.statusId,
+    });
+
+    if (!status) {
+      throw new NotFoundException(
+        `Status whit id: ${updateReservationDto.statusId} not found`,
+      );
+    }
+
+    await this.reservationRepository.update(id, {
+      ...updateReservationDto,
+      status: status,
+      ...{ statusId: undefined },
+    });
+
     return `This action updates a #${id} reservation`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
+    const reservation = await this.reservationRepository.findOneBy({
+      id: id,
+    });
+
+    if (!reservation) {
+      throw new NotFoundException(`Reservation whit id: ${id} not found`);
+    }
+
+    await this.reservationRepository.remove(reservation);
+
     return `This action removes a #${id} reservation`;
+  }
+
+  //status
+
+  async statusCreate(createStatusDto: CreateStatusDto) {
+    const newStatus = this.statusRepository.create({
+      ...createStatusDto,
+    });
+
+    return await this.statusRepository.save(newStatus);
+  }
+
+  async statusFindAll() {
+    return await this.statusRepository.find();
+  }
+
+  async statusFindOne(id: number) {
+    return await this.statusRepository.findOneBy({
+      id: id,
+    });
+  }
+
+  async statusUpdate(id: number, updateStatusDto: UpdateStatusDto) {
+    const status = await this.statusRepository.findOneBy({
+      id: id,
+    });
+
+    if (!status) {
+      throw new NotFoundException(`Status whit id: ${id} not found`);
+    }
+
+    await this.statusRepository.update(id, {
+      ...updateStatusDto,
+    });
+
+    return `This action updates a #${id} status`;
+  }
+
+  async statusRemove(id: number) {
+    const status = await this.statusRepository.findOneBy({
+      id: id,
+    });
+
+    if (!status) {
+      throw new NotFoundException(`Status whit id: ${id} not found`);
+    }
+
+    await this.statusRepository.remove(status);
+
+    return `This action removes a #${id} status`;
   }
 }
